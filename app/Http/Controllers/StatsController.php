3bc767917b1112186fb6e8719fd531dfb2d9c5cf
Kiet Pham <kietpham.dev@gmail.com>
2021-09-16 23:20:06 +1000

    init project

---
+++ app/Http/Controllers/StatsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class StatsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        $links = Link::where('user_id', $user->id)->get();
+
+        return $links->map(function (Link $link) {
+            $orders = Order::where('code', $link->code)->where('complete', 1)->get();
+
+            return [
+                'code' => $link->code,
+                'count' => $orders->count(),
+                'revenue' => $orders->sum(fn(Order $order) => $order->ambassador_revenue)
+            ];
+        });
+    }
+
+    public function rankings()
+    {
+        $ambassadors = User::ambassadors()->get();
+
+        return $ambassadors->mapWithKeys(fn(User $user) => [$user->name => (int)$user->revenue]);
+    }
+}
